5a6b5ff3c6f3d352bf00d007d7f66fcfc7655583
Gaurav Katiyar <gaurav.katiyar@knexusgroup.com>
2018-03-31 21:07:11 +0530

    made changes

--- src/utilities/RadioButton.java
+++ src/utilities/RadioButton.java
@@ -9,18 +9,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RadioButton {
 
-		public static void main(String[] args)
-		{
-			System.setProperty("webdriver.chrome.driver","/home/kumar/Selenium/chromedriver");
-			WebDriver driver=new ChromeDriver();
-			driver.get("https://www.facebook.com/");
-			List<WebElement> radio = driver.findElements(By.className("_58mt"));
-			for(int i=0;i<radio.size();i++)
-			{
-				WebElement local_radio = radio.get(i);
-				String Value=local_radio.getText();
-				System.out.println(Value);
-			}
-			//driver.close();
-		}
+		public static void main(String[] args){
+			
+					System.setProperty("webdriver.chrome.driver","/home/kumar/Selenium/chromedriver");
+					WebDriver driver = new ChromeDriver();
+					driver.get("https://www.facebook.com/");
+					List<WebElement> radio = driver.findElements(By.className("_58mt"));
+					
+					for(int i=0;i<radio.size();i++){
+						WebElement local_radio = radio.get(i);
+						String Value = local_radio.getText();
+						System.out.println(Value);
+					}
+					driver.close();
+				}
 }
