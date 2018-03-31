09ba4ba0f525b4db2375de3c679b62d2d9971684
Gaurav Katiyar <gaurav.katiyar@knexusgroup.com>
2018-03-31 23:32:17 +0530

    changes done

--- src/utilities/KeysChord.java
+++ src/utilities/KeysChord.java
@@ -13,17 +13,16 @@ public class KeysChord {
 	@Test
 	public void TestMethod() throws InterruptedException {
 		
-		System.setProperty("webdriver.chrome.driver", "/home/kumar/Selenium/chromedriver");	    
+		System.setProperty("webdriver.chrome.driver", "/home/kumar/Selenium/chromedriver");
 		WebDriver driver = new ChromeDriver();
-	    driver.get("http://www.google.com/");
-	    
-	    /*Find list of keys here
-	    * http://seleniumhq.github.io/selenium/docs/api/java/org/openqa/selenium/Keys.html*/
-	     
-	    String selectLinkOpeninNewTab = Keys.chord(Keys.CONTROL,Keys.RETURN); 
-	    driver.findElement(By.linkText("Gmail")).sendKeys(selectLinkOpeninNewTab);
-	    
-	    ArrayList<String> tab = new ArrayList<String> (driver.getWindowHandles());								
+		driver.get("http://www.google.com/");
+		
+	    /*Find list of keys here 
+	     * http://seleniumhq.github.io/selenium/docs/api/java/org/openqa/selenium/Keys.html*/
+		
+		String selectLinkOpeninNewTab = Keys.chord(Keys.CONTROL,Keys.RETURN);
+		driver.findElement(By.linkText("Gmail")).sendKeys(selectLinkOpeninNewTab);
+		ArrayList<String> tab = new ArrayList<String> (driver.getWindowHandles());
 		driver.switchTo().window(tab.get(1));
 		driver.getTitle();
 	} 
